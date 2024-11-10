47a697447fd77c62b87bd6cb9ef89ccb8b3ecd03
jiaeeooi <168224765+jiaeeooi@users.noreply.github.com>
2024-11-09 23:45:00 -0800

    Completed Enter Scheduled Transaction Page for Code 0.6

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -77,4 +77,17 @@ public class MainController {
 			e.printStackTrace();
 		}
 	}
+	
+	public void defineNewScheduledTransactionOp() {
+		URL url = getClass().getClassLoader().getResource("view/NewScheduledTransaction.fxml");
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+			if (mainBox.getChildren().size() > 1) {
+				mainBox.getChildren().remove(1);
+			}
+			mainBox.getChildren().add(pane1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
